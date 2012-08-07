cf1b43ba1c04ab51d73e064f18dbf451fc93d147
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-08-07 17:03:44 +0200

    Use augeas::lens to distribution subversion.aug

--- manifests/base.pp
+++ manifests/base.pp
@@ -9,16 +9,16 @@ class subversion::base {
         require => Package['subversion'],
     }
 
-    file { "/usr/share/augeas/lenses/contrib/subversion.aug":
+    augeas::lens {'subversion':
         ensure => present,
-        source => "puppet:///modules/subversion/subversion.aug",
+        lens_source => 'puppet:///modules/subversion/subversion.aug',
     }
 
     # only recent version of svn support the "store-password" option.
     augeas { "avoid svn password saving":
         load_path => "/usr/share/augeas/lenses/contrib/",
         context   => "/files/etc/subversion/config/auth/",
-        require   => [File["/usr/share/augeas/lenses/contrib/subversion.aug"], File["/etc/subversion"]],
+        require   => [Augeas::Lens['subversion'], File["/etc/subversion"]],
         changes   => $lsbdistcodename ? {
             default                     => "set store-auth-creds no",
             /^(Tikanga|lenny|squeeze)$/ => "set store-password no",
